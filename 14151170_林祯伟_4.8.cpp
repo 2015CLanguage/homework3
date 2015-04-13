7983e57fd6baf354fa533de87d4b1a1190c03ae4
2015CLanguage <buaa_2015_c@163.com>
2015-04-14 00:21:29 +0800

    Update 14151170_林祯伟_4.8.cpp

--- 14151170_林祯伟_4.8.cpp
+++ 14151170_林祯伟_4.8.cpp
@@ -2,15 +2,19 @@
 
 int main()
 {
-	int mark,grade;
+	int mark;
 	printf("输入百分制成绩：");
 	scanf("%d",&mark);
-	if(mark>=90) printf("成绩等级A");
-	if(mark>=80&&mark<90) printf("成绩等级B");
-	if(mark>=70&&mark<80) printf("成绩等级C");
-  if(mark>=60&&mark<70) printf("成绩等级D");
-	if(mark<60) printf("成绩等级E");
-	
+	if(mark>=0&&mark<=100){
+	    if(mark>=90) printf("成绩等级A");
+	    if(mark>=80&&mark<90) printf("成绩等级B");
+	    if(mark>=70&&mark<80) printf("成绩等级C");
+            if(mark>=60&&mark<70) printf("成绩等级D");
+	    if(mark<60) printf("成绩等级E");
+        }
+        else {
+	    printf("成绩无效。");
+	}
 	return 0; 
 
 }
