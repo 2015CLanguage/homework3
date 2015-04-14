a5d46964a962ef99771def0ff411fc8b9e703261
2015CLanguage <buaa_2015_c@163.com>
2015-04-14 21:52:37 +0800

    Create 14151186_车雪娇_4_4.c

---
+++ 14151186_车雪娇_4_4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+     int a,b,c;
+     scanf("%d,%d,%d",&a,&b,&c);
+     if(a<b)
+        if(b<c)
+        printf("max=%d\n",c);
+     else
+        printf("max=%d\n",b);
+     else if(a<c)
+        printf("max=%d\n",c);
+     else
+        printf("max=%d\n",a);
+
+    return 0;
+}
