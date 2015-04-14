1034f40d8c4415d7ce95e9a1fe5dd311a9bb1835
2015CLanguage <buaa_2015_c@163.com>
2015-04-14 14:52:44 +0800

    Create 14151133_武世杰_4.4.c

---
+++ 14151133_武世杰_4.4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("请输入三个要比较大小的整数：");
+	scanf("%d,%d,%d",&a,&b,&c);
+	if(a<b)
+	  if(b<c)
+		printf("最大的数是:%d\n",c);
+	  else
+		printf("最大的数是:%d\n",b);
+	else
+	  if(a<c)
+		printf("最大的数是:%d\n",c);
+	  else
+		printf("最大的数是:%d\n",a);
+return 0;
+}
