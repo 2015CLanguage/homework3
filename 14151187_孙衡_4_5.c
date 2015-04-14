40e129780679451bee51336d3a78532c9f317c54
2015CLanguage <buaa_2015_c@163.com>
2015-04-15 00:22:08 +0800

    Create 14151187_孙衡_4_5.c

---
+++ 14151187_孙衡_4_5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int x;
+char g;
+scanf("%d",&x);
+if(x>=90) g='A';
+else if(x>=80) g='B';
+else if(x>=70) g='C';
+else if(x>=60) g='D';
+else g='E';
+printf("%c",g);
+return 0;
+}
