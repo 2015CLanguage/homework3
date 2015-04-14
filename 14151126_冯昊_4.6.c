c88e4d16cc58f0867c97b7b029bd3e7806bd7e08
2015CLanguage <buaa_2015_c@163.com>
2015-04-14 14:40:58 +0800

    Create 14151126_冯昊_4.6.c

---
+++ 14151126_冯昊_4.6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	float x,y;
+	printf("请输入X的值\n");
+	scanf("%f",&x);
+	if(x>=10)
+		y=3*x-11;
+		else if(x>=1)
+			y=2*x-1;
+		else if(x<1)
+			y=x;
+	printf("y的值为%f\n",y);
+}
