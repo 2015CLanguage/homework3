60b6d235a28dc0cac38af2669ab263c02dd22c26
2015CLanguage <buaa_2015_c@163.com>
2015-04-14 14:22:06 +0800

    Create 14151149_吴航_4_4.c

---
+++ 14151149_吴航_4_4.c
@@ -0,0 +1,17 @@
+#clude<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("请输入三个整数:");
+	scanf("%d,%d,%d",&a,&b,&c);
+	if (a<b)
+		if (b<c)
+			printf("最大的数为%d\n",c);
+		else
+			printf("最大的数为%d\n",b);
+	else if (a<c)
+			printf("最大的数为%d\n",c);
+	else
+			printf("最大的数为%d\n",a);
+	return 0;
+}
