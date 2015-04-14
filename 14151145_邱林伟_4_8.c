fa84c3ed610ca73e6cc5a66c48329d8da9d5b93d
2015CLanguage <buaa_2015_c@163.com>
2015-04-14 14:25:59 +0800

    Create 14151145_邱林伟_4_8.c

---
+++ 14151145_邱林伟_4_8.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	printf("请输入成绩：");
+	float a;
+	scanf("%f",&a);
+	if(a>100||a<0)
+		printf("输入违法，请核实输入数据\n");
+	else
+	    if(a>=90)
+		printf("成绩等级为A\n");
+	else
+		if(a>=80)
+		printf("成绩等级为B\n");
+	else
+		if(a>=70)
+		printf("成绩等级为C\n");
+	else
+		if(a>=60)
+		printf("成绩等级为D\n");
+	else
+		printf("成绩等级为E\n");
+}
