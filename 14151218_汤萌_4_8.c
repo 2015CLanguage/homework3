89c3631d584d940870a3d7b2cde752ede05e4782
2015CLanguage <buaa_2015_c@163.com>
2015-04-14 15:07:31 +0800

    Create 14151218_汤萌_4_8.c

---
+++ 14151218_汤萌_4_8.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	float grade;
+	int a;
+	printf("Please input the grade: ");
+	scanf("%f", &grade);
+	a = grade/10;
+	switch(a)
+	{
+		case 0: 
+		case 1: 
+		case 2: 
+		case 3: 
+		case 4:
+		case 5: printf("Rate E\n"); break;
+		case 6: printf("Rate D\n"); break;
+		case 7: printf("Rate C\n"); break;
+		case 8: printf("Rate B\n"); break;
+		case 9: printf("Rate A\n"); break;
+	}
+	return 0;
+}
