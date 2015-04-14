1c4ec03b58b0d399a16a44ebfa727e5ce5a798dc
2015CLanguage <buaa_2015_c@163.com>
2015-04-14 14:28:03 +0800

    Create 14151196_孙晗宇_4.4.c

---
+++ 14151196_孙晗宇_4.4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+{
+	int a,b,c;
+	printf("请输入三个整数：");
+	scanf("%d%d%d",&a,&b,&c);
+	if(a<b){
+		a=b;
+	}
+	if(a<c){
+		a=c;
+	}
+	printf("最大的数是：%d",a);
+    return 0;
+}
+	printf(
