e16660317a15dccad0d922de42c4f69ed294fe0c
2015CLanguage <buaa_2015_c@163.com>
2015-04-14 14:47:13 +0800

    Create 14151163_马浩_4_8.c

---
+++ 14151163_马浩_4_8.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    float score;
+    char grade;
+    printf("请输入学生成绩：");
+    scanf("%f",&score);
+    while(score>100||score<0)
+      {
+      printf("输入错误，请重新输入");
+      scanf("%lf",score);
+      }
+    switch((int)(score/10))
+    {
+    case 10:
+    case 9:grade='A';break;
+    case 8:grade='B';break;
+    case 7:grade='C';break;
+    case 6:grade='D';break;
+    case 5:
+    case 4:
+    case 3:
+    case 2:
+    case 1:
+    case 0:grade='E';break;
+    }
+    printf("成绩是5.lf,成绩等级是%c\n",score,grade);
+    return 0;
+}
