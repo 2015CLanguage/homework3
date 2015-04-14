16a83cb2594948f00c32d9b84997e1b58f71e3e5
2015CLanguage <buaa_2015_c@163.com>
2015-04-14 14:55:45 +0800

    Create 14151169_谢延廷_4.4.c

---
+++ 14151169_谢延廷_4.4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{int a,b,c,t
+printf("请输入三个整数")；
+scanf("%d,%d,%d",&a,&b,&c)
+   if (a<b)
+       t=a;
+   else
+       t=b;
+   if (t<c)
+       t=c;
+   else
+       t=t;
+   printf("%d\n",t);
+return 0
+
+
+
+
+
+
+
+
+
+
+
+}
