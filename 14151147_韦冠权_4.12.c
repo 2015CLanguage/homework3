3ee83bc62672834c8db3bb92ac0411fce7bd98eb
2015CLanguage <buaa_2015_c@163.com>
2015-04-14 15:11:49 +0800

    Create 14151147_韦冠权_4.12.c

---
+++ 14151147_韦冠权_4.12.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int h;
+	float x1=2,y1=2,x2=-2,y2=2,x3=-2,y3=-2,x4=2,y4=-2,x,y,d1,d2,d3,d4;
+	printf("请分别输入一个点（x,y）的横纵坐标值：");
+	scanf("%f,%f",&x,&y);
+	d1=(x-x1)*(x-x1)+(y-y1)*(y-y1);
+	d2=(x-x2)*(x-x2)+(y-y2)*(y-y2);
+    d3=(x-x3)*(x-x3)+(y-y3)*(y-y3);
+	d4=(x-x4)*(x-x4)+(y-y4)*(y-y4);
+	if(d1<=1)
+		h=10;
+	else if(d2<=1)
+		h=10;
+	else if(d3<=1)
+		h=10;
+	else if(d4<=1)
+		h=10;
+	else
+		h=0;
+	printf("该点的高度为%d\n",h);
+	return 0;
+}
