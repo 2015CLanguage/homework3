481d9450cebb4469176d2fdb16c454e539f672f1
2015CLanguage <buaa_2015_c@163.com>
2015-04-14 14:31:52 +0800

    Create 14151166_于琦_4_12.c

---
+++ 14151166_于琦_4_12.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    float x,y,d1,d2,d3,d4;
+	scanf("(%f,%f)",&x,&y);
+	d1=(x-2)*(x-2)+(y-2)*(y-2);
+	d2=(x+2)*(x+2)+(y-2)*(y-2);
+	d3=(x+2)*(x+2)+(y+2)*(y+2);
+	d4=(x-2)*(x-2)+(y+2)*(y+2);
+	if ((d1<=1)||(d2<=1)||(d3<=1)||(d4<=1))
+		printf("h=10cm\n");
+	else
+		printf("h=0\n");
+	return 0;
+}
