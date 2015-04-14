f0a6db44d2e41323ac17bc419f033e87c478d989
2015CLanguage <buaa_2015_c@163.com>
2015-04-14 14:09:07 +0800

    Create 14151196_孙晗宇_4.6.C

---
+++ 14151196_孙晗宇_4.6.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	float x;
+	scanf("%f",&x);
+	if (x<1) 
+	printf("%f\n",x);
+	else if (x<10)  
+	printf("%f\n",2*x-1);
+	else printf("%f\n",3*x-11);
+	return 0;
+}
