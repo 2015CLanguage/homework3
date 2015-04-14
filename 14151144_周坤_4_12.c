1db501898a83522c244b6dbb65aebcc491fcb0d7
2015CLanguage <buaa_2015_c@163.com>
2015-04-14 14:48:52 +0800

    Create 14151144_周坤_4_12.c

---
+++ 14151144_周坤_4_12.c
@@ -0,0 +1,14 @@
+# homework3
+# include<stdio.h>
+int main()
+{
+float x,y;
+printf("请输入一个坐标（形如(1,2)）");
+scanf("(%f,%f)",&x,&y);
+if((x-2)*(x-2)+(y-2)*(y-2)<=1||(x-2)*(x-2)+(y+2)*(y+2)<=1||(x+2)*(x+2)+(y-2)*(y-2)<=1||(x+2)*(x+2)+(y+2)*(y+2)<=1)
+  {
+	printf("该点建筑物高度为10米\n");
+	}
+else printf("该点建筑物高度为0\n");
+return 0;
+}
