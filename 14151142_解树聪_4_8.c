8e813676656a01bc975e6d5c9c58cc8c46f429d0
2015CLanguage <buaa_2015_c@163.com>
2015-04-14 14:24:25 +0800

    Update 14151142_解树聪_4_8.c

--- 14151142_解树聪_4_8.c
+++ 14151142_解树聪_4_8.c
@@ -3,13 +3,13 @@ int main()
 {
 	float score;
 	char grade;
-	printf("请输入成绩:");
+	printf("请输入成绩:");             //提示用户输入成绩
 	scanf("%f",&score);
-	while(score<0||score>100)
+	while(score<0||score>100)          //若输入成绩不在0~100之间，重新输入，直到输入的数在0~100之间
 	{	printf("输入有误，请重新输入:");
 		scanf("%f",&score);
 	}
-	if(score>=90&&score<=100)
+	if(score>=90&&score<=100)          //分5种情况讨论，并使用用逻辑与
 	{	grade='A';
 		printf("成绩为:%.1f,对应的等级是:%c\n",score,grade);
 	}
