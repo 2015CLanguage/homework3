3c1754de6d7991b3a6679c0c1f027b95c4d4e716
2015CLanguage <buaa_2015_c@163.com>
2015-04-14 14:07:11 +0800

    Create 14151189_陆嘉伟_4_4.c

---
+++ 14151189_陆嘉伟_4_4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("请输入三个整数：");
+scanf("%d,%d,%d",&a,&b,&c);
+if(a>b)
+	if(a>c)
+		printf("最大值是%d",a);
+	else 
+		printf("最大值是%d",c);
+else if(b>c)
+	printf("最大值是%d",b);
+else
+	printf("最大值是%d",c);
+return 0;
+}
