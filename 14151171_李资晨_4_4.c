730a77e4dce015f8c04f51e010be778ea4c6bf3a
2015CLanguage <buaa_2015_c@163.com>
2015-04-16 22:12:10 +0800

    Create 14151171_李资晨_4_4.c

---
+++ 14151171_李资晨_4_4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	float a,b,c,t;
+	scanf("%f%f%f",&a,&b,&c);
+    if(a>b)
+		t=a;
+	else
+		t=b;
+	if(t>c)
+		t=t;
+	else
+		t=c;
+	printf("%5.2f\n",t);
+	return 0;
+}
