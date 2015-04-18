c60214d78885b4094198bd7fc4e7bdb572e866a7
2015CLanguage <buaa_2015_c@163.com>
2015-04-18 18:54:59 +0800

    Create 14151136_咸裕丰_4_6.c

---
+++ 14151136_咸裕丰_4_6.c
@@ -0,0 +1,16 @@
+#include"stdio.h"
+int main()
+{
+	int x,y;
+	printf("x=");
+	scanf("%d",&x);
+	if(x<1)
+		y=x;
+	else
+		if(x>=10)
+			y=3*x-11;
+		else
+			y=2*x-1;
+	printf("y=%d\n",y);
+	return 0;
+}
