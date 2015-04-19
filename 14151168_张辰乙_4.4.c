eb471ec27aa1c3dd3360e4de2ab5268e5a28395a
2015CLanguage <buaa_2015_c@163.com>
2015-04-19 10:07:43 +0800

    Create 14151168_张辰乙_4.4.c

---
+++ 14151168_张辰乙_4.4.c
@@ -0,0 +1,19 @@
+ # include <stdio.h> 
+ void main() 
+ { 
+ 	printf("请在下方输入三个整数：\n"); 
+ 	int max(int x, int y, int z); 
+ 	int a, b, c, d; 
+ 	scanf_s("%d%d%d", &a, &b, &c); 
+ 	d = max(a, b, c); 
+ 	printf("max = %d\n", d); 
+ } 
+ int max(int x, int y, int z) 
+ { 
+ 	int m, n; 
+ 	if (x>y) m = x; 
+ 	else m = y; 
+ 	if (m>z) n = m; 
+ 	else n = z; 
+ 	return(n); 
+ } 
