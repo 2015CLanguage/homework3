c25d6601cd0869ef7d5bb3650de3a29c6e3e2fa5
2015CLanguage <buaa_2015_c@163.com>
2015-04-19 10:11:45 +0800

    Create 14151168_张辰乙_4.8.c

---
+++ 14151168_张辰乙_4.8.c
@@ -0,0 +1,23 @@
+ +#include <stdio.h> 
+ +void main() 
+ +{ 
+ +	int c; 
+ +	printf("请输入你的分数\n"); 
+ +	scanf("%d",&c); 
+ +	if (c<0 || c>100) 
+ +		printf("error,please input again\n"); 
+ +	else 
+ +	{ 
+ +		printf("你的分数等级是\n"); 
+ +	    c=c/10; 
+ +	    switch(c) 
+ +	    { 
+ +		    case 10:printf("A\n");break; 
+ +		    case 9:printf("A\n");break; 
+ +		    case 8:printf("B\n");break; 
+ +		    case 7:printf("C\n");break; 
+ +		    case 6:printf("D\n");break; 
+ +		    default:printf("E\n"); 
+ +	    } 
+ +	} 
+ +} 
